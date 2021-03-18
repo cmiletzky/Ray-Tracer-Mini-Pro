0947c7cfb6e2a72a5ec3fb1e5285ff428add35e0
Garusi <mgarusi101@gmail.com>
2021-03-18 13:42:00 +0200

    fixed Plane class

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -17,31 +17,23 @@ public class Plane implements Geometry {
 	 * updated - by the tirgul
 	 */
 	public Plane(Point3D p1, Point3D p2,Point3D p3) {
-			q0 = new Vector(p1);
-			Vector vec1 = q0.subtract(p2);
-			Vector vec2 = q0.subtract(p3);
-			if ((vec1.crossProduct(vec2)).dotProduct(new Vector(q0)) == 0)
-			{ normal = (vec1.crossProduct(vec2).normalize());}
-			else System.out.println(" " +(vec1.crossProduct(vec2)));
-	
+			normal = null;
+			q0= p1;
+			
 	}
 	
 	/**
 	 * point and normal vector constructor 
 	 */
 	public Plane(Point3D p1, Vector v1) {
-		q0 = new Point3D(p1);
-		normal = new Vector(v1);
+		q0 = p1;
+		normal = new Vector(v1.getHead());
 	}
 	
 	
 	
 		
-//	public Plane(Vector vector, Point3D point3d) {
-//		// TODO Auto-generated constructor stub
-//		q0=new Point3D(point3d);
-//		normal=vector;
-//	}
+
 
 	/**
 	 * to string
@@ -65,11 +57,4 @@ public class Plane implements Geometry {
 		// TODO Auto-generated method stub
 		return normal;
 	}
-
-//	@Override
-//	public List<Point3D> findIntsersections(Ray ray) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-	
 }
