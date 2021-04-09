cf4fc13b5b7e38d5e7ca87497fd9743c71d5667d
chaim miletzky <chyikol@gmail.com>
2021-04-09 17:55:34 +0300

    test for sphere

--- src/unittests/SphereTests.java
+++ src/unittests/SphereTests.java
@@ -9,12 +9,12 @@ import primitives.Vector;
 public class SphereTests {
 
     // ============ Equivalence Partitions Tests ==============
-
+	// need check only one case
 	@Test
 	public void testGetNormal() {
 		Sphere s = new Sphere(1, new Point3D(0,0,0));
-		//Vector ans = new Vector(x1, y1, z1)
-		//assertEquals(,ne`jjjj );
+		Vector ans = new Vector(1, 1, 1);
+		assertTrue(s.getNormal(new Point3D(1.0, 1.0, 1.0)).equals(ans));
 		
 	}
 
--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -3,14 +3,15 @@
  */
 package unittests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Vector;
 
 import org.junit.Test;
 
 import geometries.Triangle;
-import primitives.*;
+import primitives.Point3D;
+//import primitives.Vector;
 
 /**
  * @author חייקי
@@ -25,7 +26,9 @@ public class TriangleTest {
 	public void testGetNormal() {
 		// one equivalent partition, no boundary cases
 		Triangle t= new Triangle(new Point3D(0,0,0),new Point3D(1,2,0) , new Point3D(2,0,0));
-		assertEquals(new primitives.Vector(0, 0, -4), t.getNormal(new Point3D(1, 1, 1)).normalize());
+		primitives.Vector ans1=new primitives.Vector(0.0, 0.0, -1.0);
+		primitives.Vector ans2=new primitives.Vector(0.0, 0.0, 1.0);
+		assertTrue((t.getNormal(new Point3D(1, 1, 1)).equals(ans1) ||t.getNormal(new Point3D(1, 1, 1)).equals(ans2)));
 	}
 
 }
