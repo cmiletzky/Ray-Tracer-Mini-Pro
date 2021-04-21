274a383e5a76c33f91082d41b51616d33d58b42a
chaim miletzky <chyikol@gmail.com>
2021-04-21 18:30:21 +0300

    edit task 4

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -22,15 +22,24 @@ public class Camera {
 	private double height;
 	private double distance;
 	
-	public Camera(Point3D zero, Vector vector, Vector vector2) {
+	public Camera(Point3D p0, Vector vTo, Vector vUp) {
 
-	if (!isZero) {
+	if (!isZero(vUp.dotProduct(vTo))) {
 		
+		throw new IllegalArgumentException("vUp and vTo must be orthogonal")
+	  }
+	this.p0=p0;
+	this.vTo=vTo.normalized();
+	this.vUp=vUp.normalized();
+	vRight= vTo.crossProduct(vUp)p0;
+	
 	}
-	}
 
 
-
+/**
+ * getters for fields 
+ * @return
+ */
 	public Vector getvUp() {
 		return vUp;
 	}
@@ -47,9 +56,38 @@ public class Camera {
 		return vRight;
 	} 
 	
+	
+	/**
+	 *  setters for view plan and distance by chaning
+	 * @param width
+	 * @param height
+	 * @return
+	 */
+	public Camera setViewPlaneSize(double width,double height) {
+		this.width = width;
+		this.height = height;
+		return this;
+	}
+
+
+	public Camera setDistance(double distance) {
+		this.distance = distance;
+		return this;
+	}
+
+
+
 	Ray constructRay(int nx, int ny, int i, int j)
 	{
+		Point3D pc= p0.add(vTo.scale(distance));
+		double Rx = width/nx;
+		double Ry = height/ny;
 		
+		
+		Point3D pij = pc;
+		double yi = -Ry * (i - (ny - 1)/2d);
+		double xj = Rx * (i - (nx - 1)/2d);
+
 	}
 
 
