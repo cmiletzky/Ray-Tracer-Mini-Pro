bc7b6de0cc62b2652dc337f2539320e048d0e849
chaim miletzky <chyikol@gmail.com>
2021-04-28 18:24:19 +0300

    final task 5

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -21,41 +21,6 @@ public class Render {
 	RayTracerBase rayTracerBase = null;
 
 	
-	 public void renderImage() {
-		 try {
-			 if (imageWriter==null) {
-				throw new MissingResourceException("missing resource", ImageWriter.class.getCanonicalName(), "");
-			}
-			 if (scene==null) {
-					throw new MissingResourceException("missing resource", Scene.class.getCanonicalName(), "");
-				}
-			 if (camera==null) {
-					throw new MissingResourceException("missing resource", Camera.class.getCanonicalName(), "");
-				}
-			 if (rayTracerBase==null) {
-					throw new MissingResourceException("missing resource", RayTracerBase.class.getCanonicalName(), "");
-				}
-			 // if all right ... rendering the image 
-			 int Nx = imageWriter.getNx();
-			 int Ny = imageWriter.getNy();
-			 for (int i = 0; i < Ny; i++) {
-				 for (int j = 0; j < Nx; j++) {
-					
-					 Ray ray = camera.constructRayThroughPixel(Nx, Ny, j, i);
-					 primitives.Color pixelColor = rayTracerBase.traceRay(ray);
-					 imageWriter.writePixel(j,i,pixelColor);
-				}
-			}
-			
-		} catch (MissingResourceException e) {
-			throw new UnsupportedOperationException( e.getClassName() + " not initialized");
-		}
-	 }
-	
-	
-	
-	
-	
 	/**
 	 * @param imageWriter the imageWriter to set
 	 */
@@ -112,4 +77,73 @@ public class Render {
 		return rayTracerBase;
 	}
 
+	
+	
+	
+	
+	/**
+	 * use with all element to write the details into "one box"
+	 */
+
+	
+	 public void renderImage() {
+		 try {
+			 if (imageWriter==null) {
+				throw new MissingResourceException("missing resource", ImageWriter.class.getCanonicalName(), "");
+			}
+			 if (scene==null) {
+					throw new MissingResourceException("missing resource", Scene.class.getCanonicalName(), "");
+				}
+			 if (camera==null) {
+					throw new MissingResourceException("missing resource", Camera.class.getCanonicalName(), "");
+				}
+			 if (rayTracerBase==null) {
+					throw new MissingResourceException("missing resource", RayTracerBase.class.getCanonicalName(), "");
+				}
+			 // if all right ... rendering the image 
+			 int Nx = imageWriter.getNx();
+			 int Ny = imageWriter.getNy();
+			 for (int i = 0; i < Ny; i++) {
+				 for (int j = 0; j < Nx; j++) {
+					
+					 Ray ray = camera.constructRayThroughPixel(Nx, Ny, j, i);
+					 primitives.Color pixelColor = rayTracerBase.traceRay(ray);
+					 imageWriter.writePixel(j,i,pixelColor);
+				}
+			}
+			
+		} catch (MissingResourceException e) {
+			throw new UnsupportedOperationException( e.getClassName() + " not initialized");
+		}
+	 }
+	
+	
+	/**
+	 * printing grid for work with it
+	 * @param interval
+	 * @param color
+	 */
+	public void printGrid( int interval, primitives.Color color)
+	{
+		int Nx = imageWriter.getNx();
+		int Ny = imageWriter.getNy();
+		
+		for (int i = 0; i < Ny; i++) {
+				for (int j = 0; j < Nx; j++) {
+					if (i % interval == 0 || j % interval == 0) {
+						imageWriter.writePixel(j, i, color);
+					}
+				}
+		}
+
+		
+	}
+	/**
+	 *  Finally use with imagewriter to draw the image builded
+	 */
+	public void writeToImage() {
+		imageWriter.writeToImage();
+		
+	}
+	
 }
